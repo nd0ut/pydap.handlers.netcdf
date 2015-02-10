3f35c18dc14f533c266370889fffa51be6a730da
nd0ut <nd0ut.me@gmail.com>
2015-02-10 18:23:45 +0300

    fix entry point

--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@ setup(name='pydap.handlers.netcdf',
     version=version,
     description="Netcdf handler for Pydap",
     long_description="""
-This handler allows Pydap to serve data from NetCDF files. By default           
-`pupynere <http://code.dealmeida.net/pupynere>`_ is installed as a dependency,    
-providing support for NetCDF 3 files. If you also want to server NetCDF 4 files 
+This handler allows Pydap to serve data from NetCDF files. By default
+`pupynere <http://code.dealmeida.net/pupynere>`_ is installed as a dependency,
+providing support for NetCDF 3 files. If you also want to server NetCDF 4 files
 you'll need to install the `netCDF4-python <https://code.google.com/p/netcdf4-python/>`_
 library.
 """,
@@ -39,6 +39,6 @@ library.
     install_requires=install_requires,
     entry_points="""
         [pydap.handler]
-        netcdf = pydap.handlers.netcdf:NetCDFHandler
+        netcdf = pydap.handlers.netcdf:Handler
     """,
 )
